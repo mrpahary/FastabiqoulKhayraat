d26da5644746cfe9dd601411b624ce2e3ae59435
mrpahary <mrpahary@gmail.com>
2026-04-03 16:08:13 +0400

    Create Anaasheed.js component file

---
+++ src/components/Anaasheed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Anaasheed = () => {
+    return (
+        <div>
+            <h1>Anaasheed Component</h1>
+        </div>
+    );
+};
+
+export default Anaasheed;
